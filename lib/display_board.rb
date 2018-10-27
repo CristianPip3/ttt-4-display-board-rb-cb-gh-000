2c096de72b4e21e1bef994649c0ded9ce0b26865
Cristian Pip3 <cristianbenavides@unicauca.edu.co>
2018-10-27 17:53:31 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,6 +5,8 @@ def display_board(board = [" "," "," "," "," "," "," "," "," "])
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 
 
 end
